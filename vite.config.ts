8382273d97db88aa2c0763a25669065b50b47af6
Lília Paula <liliaxtz2@gmail.com>
2023-09-29 21:39:33 -0300

    chore: test setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    css: false,
+    deps: {
+      inline: ["./src/test/setup.ts"],
+    },
+    setupFiles: ["./src/test/setup.ts"],
+    reporters: "dot",
+  },
+});
